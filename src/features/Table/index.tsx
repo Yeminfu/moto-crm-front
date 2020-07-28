93b73dc173d5f7cc63f4697a0cbce2d95868edea
Yeminfu <yeminfu4319@gmail.com>
2020-07-28 17:03:12 +1000

    upd year report

--- src/features/Table/index.tsx
+++ src/features/Table/index.tsx
@@ -4,64 +4,67 @@ import { Cart } from "./atoms/cart";
 
 export const Table = ({ reportData }: any) => (
   <div style={{ overflowX: "scroll" }}>
-    {/* <pre>{JSON.stringify({ reportData }, null, " ")}</pre> */}
-    <Btable striped bordered hover size="sm">
-      <thead style={{ whiteSpace: "nowrap" }}>
-        <tr>
-          {reportData.map((x: any) =>
-            x.head_rows[0].map((y: any, i: number) => (
-              <th colSpan={y.colSpan} rowSpan={y.rowSpan} key={i}>
-                {y.text}
-              </th>
-            ))
-          )}
-          <th rowSpan={3} />
-        </tr>
-        <tr>
-          {reportData.map((x: any) =>
-            x.head_rows[1].map((y: any, i: number) => (
-              <th colSpan={y.colSpan} rowSpan={y.rowSpan} key={i}>
-                {y.text}
-              </th>
-            ))
-          )}
-        </tr>
-        <tr>
-          {reportData.map((x: any) =>
-            x.head_rows[2].map((y: any, i: number) => (
-              <th colSpan={y.colSpan} rowSpan={y.rowSpan} key={i}>
-                {y.text}
-              </th>
-            ))
-          )}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {reportData.map((x: any) =>
-            x.body_cols.map((y: any, i: number) => (
-              <td key={i}>
-                {((type) => {
-                  switch (type) {
-                    case "image":
-                      return (
-                        <img
-                          src={typeof y.data === "string" ? y.data : ""}
-                          alt=""
-                        />
-                      );
-                    default:
-                      return y.data;
-                  }
-                })(y.type)}
+    {reportData.length && (
+      <Btable striped bordered hover size="sm">
+        <thead style={{ whiteSpace: "nowrap" }}>
+          <tr>
+            {reportData[0].map((x: any) =>
+              x.head_rows[0].map((y: any, i: number) => (
+                <th colSpan={y.colSpan} rowSpan={y.rowSpan} key={i}>
+                  {y.text}
+                </th>
+              ))
+            )}
+            <th rowSpan={3} />
+          </tr>
+          <tr>
+            {reportData[0].map((x: any) =>
+              x.head_rows[1].map((y: any, i: number) => (
+                <th colSpan={y.colSpan} rowSpan={y.rowSpan} key={i}>
+                  {y.text}
+                </th>
+              ))
+            )}
+          </tr>
+          <tr>
+            {reportData[0].map((x: any) =>
+              x.head_rows[2].map((y: any, i: number) => (
+                <th colSpan={y.colSpan} rowSpan={y.rowSpan} key={i}>
+                  {y.text}
+                </th>
+              ))
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {reportData.map((row: any, ind: any) => (
+            <tr key={ind}>
+              {row.map((x: any) =>
+                x.body_cols.map((y: any, i: number) => (
+                  <td key={i}>
+                    {((type) => {
+                      switch (type) {
+                        case "image":
+                          return (
+                            <img
+                              src={typeof y.data === "string" ? y.data : ""}
+                              alt=""
+                            />
+                          );
+                        default:
+                          return y.data;
+                      }
+                    })(y.type)}
+                  </td>
+                ))
+              )}
+              <td>
+                <Cart />
               </td>
-            ))
-          )}
-          <td>
-            <Cart />
-          </td>
-        </tr>
-      </tbody>
-    </Btable>
+            </tr>
+          ))}
+        </tbody>
+      </Btable>
+    )}
   </div>
 );
